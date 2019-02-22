dd966205c7972daa211d51cd29388d23caf94a89
ronakruparel <rr9323@gmail.com>
2019-02-22 19:00:21 +0530

    fix(headlines):fixed data render of top-headlines

--- components/Headlines.js
+++ components/Headlines.js
@@ -1,16 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-
+import { ScrollView, Text, View, Image } from "react-native";
+const styles = {
+    title: {
+        fontWeight: "bold",
+        textAlign: "center",
+        width: "60%"
+    },
+    image: {
+        height: 200,
+        width: 300,
+        marginLeft: "auto",
+        marginRight: "auto"
+    }
+};
 export default class Apps extends React.Component {
     render() {
         const { topheadlines } = this.props;
-        topheadlines.data.map(headlines => console.log(headlines));
+        console.log(topheadlines);
         return (
-            <View>
+            <ScrollView>
                 {topheadlines.data.map(headlines => {
-                    return <Text>{headlines.title}</Text>;
+                    return (
+                        <View key={headlines.title}>
+                            <Image
+                                source={{ uri: headlines.urlToImage }}
+                                style={styles.image}
+                            />
+                            <Text style={styles.title}>{headlines.title}</Text>
+                            <Text>{headlines.url}</Text>
+                            <Text>{headlines.description}</Text>
+                        </View>
+                    );
                 })}
-            </View>
+            </ScrollView>
         );
     }
 }
